15a804beae8836839b26e0a78205089c0bc4ece8
EC2 Default User <ec2-user@ip-172-31-83-11.ec2.internal>
2022-05-02 11:10:40 +0000

    コメント機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'book_comments/create'
+  get 'book_comments/destroy'
   root to: "home#top"
   devise_for :users
   resources :books, only: [:create, :index, :show,:edit,:destroy,:update] do
     resources :favorites, only: [:create, :destroy]
+    resources:book_comments,only:[:create,:destroy]
   end
   resources :users, only: [:show, :edit,:update,:index,:create]
   
